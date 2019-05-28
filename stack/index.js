b593319508e5bd255de15bbf3a3346882f236a13
半梦半醒 <feying1221@126.com>
2019-05-28 11:38:13 +0800

    栈

--- stack/index.js
+++ stack/index.js
@@ -22,8 +22,28 @@ class Stack {
     return this.items.length;
   }
   print() {
-    return this.items.toString(',');
+    return this.items.join('');
   }
 }
 
-const stack = new Stack();
+/**************** demo01 ****************/
+/**
+ * 十进制转二进制
+ * 思路：将十进制数字和2整除（二进制满二进一），直到结果是0位止
+ */
+const divideBy2 = number => {
+  let stack = new Stack(),
+    rem,
+    binaryString;
+
+  while (number > 0) {
+    stack.push(Math.floor(number % 2));
+    number = Math.floor(number / 2);
+  }
+
+  if (!stack.empty()) {
+    const stackItemSting = stack.print();
+    binaryString = [...stackItemSting].reverse().join('');
+  }
+  return binaryString;
+}
